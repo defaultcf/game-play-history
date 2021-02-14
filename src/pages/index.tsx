07c663f8b813d337e012b9f26837e7cad28f4a6f
i544c <leo@i544c.me>
2021-02-15 02:54:43 +0900

    Add simplified view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,41 @@
 import React from 'react';
-import { PageProps } from 'gatsby';
+import { graphql } from 'gatsby';
 
 import Title from '@/components/Title';
 
-const Home: React.FC<PageProps> = () => (
+type Props = {
+  data: {
+    allHistoryJson: {
+      nodes: Array<{
+        name: string;
+        playtime_forever: number;
+      }>
+    }
+  }
+};
+
+const Home: React.FC<Props> = ({ data }) => (
   <main>
     <Title />
-    <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
-    <p>
-      Follow me on Twitter (
-      <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
-    </p>
+    <ul>
+      {data.allHistoryJson.nodes.map((node, key) => (
+        <li key={key}>
+          {node.name}, {(node.playtime_forever / 60).toFixed(1)}時間
+        </li>
+      ))}
+    </ul>
   </main>
 );
+
+export const query = graphql`
+  query {
+    allHistoryJson(sort: {fields: playtime_forever, order: DESC}) {
+      nodes {
+        name
+        playtime_forever
+      }
+    }
+  }
+`;
 
 export default Home;
