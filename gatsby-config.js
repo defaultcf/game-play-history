17b7db429bf0e2caf4965e3f972246aaa570a565
i544c <leo@i544c.me>
2021-02-15 01:34:03 +0900

    Add history.json to GraphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,13 @@
 module.exports = {
   // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
   // don't need to define it here (just if you need to change the options)
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/data/`,
+      },
+    },
+    `gatsby-transformer-json`
+  ],
 };
